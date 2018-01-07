876266369a80b3ec73ae658582a38fc4d5082d1c
alitavafi <ali.tavafi9@gmail.com>
2018-01-07 18:11:56 +0330

    setServer() / setBaseUrl() / getBaseUrl() methods have been created

--- Telegro.php
+++ Telegro.php
@@ -1,12 +1,30 @@
 <?php
 class Telegro
 {
-    CONST BASE_URL = "https://api.telegram.org/bot";
+    public $base_url = "https://api.telegram.org/bot";
     private $server;
+    private $bot_token;
 
     public function __construct($bot_token)
     {
-        $this->server = self::BASE_URL . $bot_token;
+    	$this->bot_token = $bot_token;
+        $this->setServer($this->base_url);
+    }
+
+    private function setServer($base_url)
+    {
+    	$this->server = $this->base_url . $this->bot_token;
+    }
+
+    public function setBaseUrl($base_url)
+    {
+    	$this->base_url = $base_url;
+    	$this->setServer($base_url);
+    }
+
+    public function getBaseUrl()
+    {
+    	return $this->base_url;
     }
 }
 ?>
